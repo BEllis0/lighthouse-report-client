487b46b6a75488bfef68bcc180575df4d28404f8
Brandon Ellis <brandonellis.email@gmail.com>
2021-04-16 14:03:12 -0600

    changed response type to blob

--- src/api/downloadReport.js
+++ src/api/downloadReport.js
@@ -1,7 +1,10 @@
 import axios from 'axios';
 export function downloadReport(url) {
     return new Promise((resolve, reject) => {
-        let requestDownload = axios.get(`https://lighthouse-report-files.herokuapp.com/api/reports/all?url=${url}`)
+        let requestDownload = axios({
+            url: `https://lighthouse-report-files.herokuapp.com/api/reports/all?url=${url}`,
+            responseType: 'blob'
+        })
             .then(response => {
                 resolve(response);
             })
